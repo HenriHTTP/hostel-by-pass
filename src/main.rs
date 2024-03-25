555a653876381dd67a84b88dd1dcad04f1acd8b0
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-03-25 12:45:14 -0300

    refactor: fix error generator routes and fallback implements

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::net::SocketAddr;
-use axum::{routing::Route, server};
+use axum::{Router, Server};
 use axum::http::StatusCode;
+use axum::Json;
 use serde_json::Value;
 use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    let api = Route::new().fallback(api_fallback);
-    let app = Route::new().nest("/api", api);
+    let api: Router = Router::new().fallback(api_fallback());
+    let app: Router = Router::new().nest("/api", api);
     let address: SocketAddr =
         SocketAddr::from(([127, 0, 0, 1], 8080));
 
-    let server = server::bind(&address).serve(app.into_make_service());
+    let server = Server::bind(&address).serve(app.into_make_service());
 
     server.await.unwrap();
-
 }
 
 fn api_fallback() -> (StatusCode, Json<Value>) {
@@ -23,5 +23,5 @@ fn api_fallback() -> (StatusCode, Json<Value>) {
         "message": "route is not found,try again more late"
     });
 
-    (StatusCode::NOT_FOUND, Json(body))
+    (StatusCode::NOT_FOUND, Json(body_response))
 }
