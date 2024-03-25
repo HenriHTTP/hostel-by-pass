b809963ceab6862b2c50797c6bbf5cb1095b446f
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-03-25 12:48:54 -0300

    refactor: fix error api_fallback should async function

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,15 @@ use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    let api: Router = Router::new().fallback(api_fallback());
+    let api: Router = Router::new().fallback(api_fallback);
     let app: Router = Router::new().nest("/api", api);
-    let address: SocketAddr =
-        SocketAddr::from(([127, 0, 0, 1], 8080));
-
+    let address: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let server = Server::bind(&address).serve(app.into_make_service());
-
     server.await.unwrap();
 }
 
-fn api_fallback() -> (StatusCode, Json<Value>) {
-    let body_response = json!({
+async  fn api_fallback() -> (StatusCode, Json<Value>) {
+    let body_response: Value = json!({
         "status": 404,
         "message": "route is not found,try again more late"
     });
