2918f3fb818fbcfe67bf961a5b29dda72bd9e555
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-03-27 21:59:04 -0300

    feat: create create reservation function

--- src/controller/create_reservation.rs
+++ src/controller/create_reservation.rs
@@ -1,3 +1,17 @@
-pub fn hello_world(){
+pub fn hello_world() {
     println!("hello world, create_reservation module!!");
+}
+
+use axum::{http::{StatusCode}, response::IntoResponse, Json, Extension};
+use crate::entity::reservation::Reservation;
+use crate::repository::reservation_repository::ReservationRepository;
+
+pub async fn create_reservation(
+    Json(reservation): Json<Reservation>,
+    repository: Extension<ReservationRepository>,
+) -> impl IntoResponse {
+    match repository.insert(reservation).await {
+        Ok(()) => StatusCode::CREATED,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
 }
