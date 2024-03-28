5fb06c149b1edb8fc92b2519301c0f649ad03d51
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-03-27 22:00:59 -0300

    feat: create insert and new functions

--- src/repository/reservation_repository.rs
+++ src/repository/reservation_repository.rs
@@ -1,6 +1,17 @@
 use mongodb::Collection;
+use mongodb::error::Error;
 use crate::entity::reservation::Reservation;
 
 pub struct ReservationRepository {
     collection: Collection<Reservation>,
+}
+
+impl ReservationRepository {
+    pub fn new(collection: Collection<Reservation>) -> Self{
+        ReservationRepository { collection }
+    }
+    pub async fn insert(&self, reservation: Reservation) -> Result<(),Error>{
+        self.collection.insert_one(reservation, None).await?;
+        Ok(())
+    }
 }
