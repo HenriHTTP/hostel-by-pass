5d499e9878ebd02ade2cba3f2c7970f0504fb8e7
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-03-29 11:57:26 -0300

    refactor: import modules

--- src/controller/create_reservation.rs
+++ src/controller/create_reservation.rs
@@ -2,16 +2,16 @@ pub fn hello_world() {
     println!("hello world, create_reservation module!!");
 }
 
-use axum::{http::{StatusCode}, response::IntoResponse, Json, Extension};
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::Json;
+use axum::Extension;
 use crate::entity::reservation::Reservation;
 use crate::repository::reservation_repository::ReservationRepository;
 
-pub async fn create_reservation(
-    Json(reservation): Json<Reservation>,
-    repository: Extension<ReservationRepository>,
-) -> impl IntoResponse {
+pub async fn create_reservation(Json(reservation): Json<Reservation>, repository: Extension<ReservationRepository>, ) -> impl IntoResponse {
     match repository.insert(reservation).await {
-        Ok(()) => StatusCode::CREATED,
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        Ok(()) => StatusCode::CREATED.into_response(),
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
