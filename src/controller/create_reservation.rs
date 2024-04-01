0eec2ae9307023c65463316098043ed20f1b1126
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-01 20:21:31 -0300

    feat: fix and implement create reservation function

--- src/controller/create_reservation.rs
+++ src/controller/create_reservation.rs
@@ -4,10 +4,35 @@ use axum::Json;
 use axum::Extension;
 use crate::entity::reservation::Reservation;
 use crate::repository::reservation_repository::ReservationRepository;
+use axum::extract::Request;
+use serde_json::json;
+use serde_json::Value;
 
-pub async fn create_reservation(Json(reservation): Json<Reservation>, repository: Extension<ReservationRepository>, ) -> impl IntoResponse {
-    match repository.insert(reservation).await {
-        Ok(()) => StatusCode::CREATED.into_response(),
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+pub async fn create_reservation(Json(req): Json<Reservation>) -> impl IntoResponse {
+    let repository = ReservationRepository::new(
+        "mongodb://localhost:27021",
+        "hostel-by-pass",
+        "reservations",
+    ).await;
+
+    let error: Value = json!({
+        "error": {
+        "code": 404,
+        "message": "Route not found. Please check the URL and try again later."
+        }
+    });
+
+    let success: Value = json!({
+        "code":200,
+        "message": "reservation create with success."
+    });
+
+    if let Ok(repository) = repository {
+        if let Err(_) = repository.insert(req).await {
+            return (StatusCode::INTERNAL_SERVER_ERROR, Json(error));
+        }
+        return (StatusCode::CREATED, Json(success));
     }
+
+    (StatusCode::INTERNAL_SERVER_ERROR, Json(error))
 }
