1a2b02d6b710f13909103d7e32ad020fca155ae8
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-01 20:21:47 -0300

    feat: fix env variables

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,12 @@ use std::env;
 
 #[tokio::main]
 async fn main() {
-    let host: String = env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-    let port: String= env::var("PORT").unwrap_or_else(|_| "8080".to_string());
-    let addr: String= format!("{}:{}", host, port);
+    dotenv().ok();
+    let host: String = env::var("HOST").unwrap_or_default();
+    let port: String = env::var("PORT").unwrap_or_default();
+    let addr: String = format!("{}:{}", host, port);
     let app: Router = Router::new().nest("/api", routes());
-    let address : TcpListener = TcpListener::bind(&addr).await.unwrap();
+    let address: TcpListener = TcpListener::bind(&addr).await.unwrap();
     let server = serve(address, app);
     println!("app listening at http://localhost:{}", &addr);
     server.await.unwrap();
