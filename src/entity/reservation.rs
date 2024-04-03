ef59bd1e4f8cb13a0432fc6afbc8bc38ca8ac826
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-02 21:26:09 -0300

    feat: remove to document function

--- src/entity/reservation.rs
+++ src/entity/reservation.rs
@@ -1,7 +1,6 @@
 use serde::Deserialize;
 use serde::Serialize;
 use mongodb::bson::doc;
-use mongodb::bson::Document;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Reservation {
@@ -12,18 +11,4 @@ pub struct Reservation {
     check_out_date: String,
     num_guests: u32,
     special_requests: Option<Vec<String>>,
-}
-
-impl Reservation {
-    pub fn to_document(&self) -> Document {
-        let document: Document =  doc! {
-            "name": &self.name,
-            "email": &self.email,
-            "room_number": &self.room_number,
-            "check_in_date": &self.check_in_date,
-            "check_out_date": &self.check_out_date,
-            "num_guests": &self.num_guests,
-        };
-        document
-    }
 }
