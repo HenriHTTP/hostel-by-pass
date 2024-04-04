6576ca36e6d94cc045a927574620b5eb571238f8
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-04 15:58:00 -0300

    feat: create email validation function

--- src/controller/create_reservation.rs
+++ src/controller/create_reservation.rs
@@ -1,21 +1,25 @@
+use crate::entity::reservation::Reservation;
+use crate::repository::reservation_repository::ReservationRepository;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
-use crate::entity::reservation::Reservation;
-use crate::repository::reservation_repository::ReservationRepository;
 use serde_json::json;
 use serde_json::Value;
 use std::env;
+use mongodb::error::Error;
+use regex::Regex;
 
-
-pub async fn create_reservation(Json(req): Json<Reservation>) -> impl IntoResponse {
-    if let Err(error) = is_valid_reservation(&req).await {
+pub async fn create_reservation(Json(reservation): Json<Reservation>) -> impl IntoResponse {
+    if let Err(error) = is_valid_reservation(&reservation).await {
         return error;
+    }
+    if let Err(error) = is_valid_email(&reservation).await{
+        return error
     }
     let collection_name: String = env::var("COLLECTION_NAME").unwrap_or_default();
     let db_name: String = env::var("DATABASE_NAME").unwrap_or_default();
     let uri: String = env::var("MONGO_URL").unwrap_or_default();
-    let repository = ReservationRepository::new(
+    let repository: Result<ReservationRepository, Error> = ReservationRepository::new(
         &uri,
         &db_name,
         &collection_name,
@@ -31,7 +35,7 @@ pub async fn create_reservation(Json(req): Json<Reservation>) -> impl IntoRespon
         "message": "reservation create with success."
     });
     if let Ok(repository) = repository {
-        if let Err(_) = repository.insert(req).await {
+        if let Err(_) = repository.insert(reservation).await {
             return (StatusCode::INTERNAL_SERVER_ERROR, Json(error));
         }
         return (StatusCode::CREATED, Json(success));
@@ -60,3 +64,19 @@ async fn is_valid_reservation(reservation: &Reservation) -> Result<(), (StatusCo
     Ok(())
 }
 
+async fn is_valid_email(reservation: &Reservation) -> Result<(), (StatusCode, Json<Value>)> {
+    let valid_email_pattern: Regex = Regex::new(
+        r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+        .unwrap();
+    let email_is_valid : bool = valid_email_pattern.is_match(&reservation.email);
+    if !email_is_valid{
+        let error: Value = json!({
+        "error": {
+        "code": 400,
+        "message": "this email is not valid try use a valid email!"
+        }
+    });
+        return Err((StatusCode::BAD_REQUEST, Json(error)));
+    }
+    Ok(())
+}
