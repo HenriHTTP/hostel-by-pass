26715bbfff0b86faca09415239b0f06a8b7834fe
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-08 21:04:10 -0300

    feat:add get reservation by email

---
+++ src/controller/get_reservation_by_email.rs
@@ -0,0 +1,37 @@
+use crate::entity::email_reservation::ReservationEmail;
+use crate::repository::reservation_repository::ReservationRepository;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::Json;
+use serde_json::json;
+use serde_json::Value;
+use std::env;
+use mongodb::error::Error;
+
+pub async fn get_reservation_by_email(Json(reservation_email): Json<ReservationEmail>) ->impl IntoResponse{
+    let collection_name: String = env::var("COLLECTION_NAME").unwrap_or_default();
+    let db_name: String = env::var("DATABASE_NAME").unwrap_or_default();
+    let uri: String = env::var("MONGO_URL").unwrap_or_default();
+    let repository: Result<ReservationRepository, Error> = ReservationRepository::new(
+        &uri,
+        &db_name,
+        &collection_name,
+    ).await;
+    let error: Value = json!({
+        "error": {
+        "code": 404,
+        "message": "there was an unexpected error please try later."
+        }
+    });
+    let success: Value = json!({
+        "code":200,
+        "message": "query success"
+    });
+    match repository {
+        Ok(repository) => {
+            let _result_from_email : Vec<String> = repository.get_reservation_from_email(reservation_email).await.unwrap();
+            (StatusCode::CREATED, Json(success))
+        }
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(error)),
+    }
+}
