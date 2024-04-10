46b3fcc785b453556dd138d3e1db805802d1ba9d
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-10 20:35:17 -0300

    refactor: fix return json implement

--- src/controller/get_reservation_by_email.rs
+++ src/controller/get_reservation_by_email.rs
@@ -8,7 +8,7 @@ use serde_json::Value;
 use std::env;
 use mongodb::error::Error;
 
-pub async fn get_reservation_by_email(Json(reservation_email): Json<ReservationEmail>) ->impl IntoResponse{
+pub async fn get_reservation_by_email(Json(reservation_email): Json<ReservationEmail>) -> impl IntoResponse {
     let collection_name: String = env::var("COLLECTION_NAME").unwrap_or_default();
     let db_name: String = env::var("DATABASE_NAME").unwrap_or_default();
     let uri: String = env::var("MONGO_URL").unwrap_or_default();
@@ -17,21 +17,20 @@ pub async fn get_reservation_by_email(Json(reservation_email): Json<ReservationE
         &db_name,
         &collection_name,
     ).await;
-    let error: Value = json!({
-        "error": {
-        "code": 404,
-        "message": "there was an unexpected error please try later."
-        }
-    });
-    let success: Value = json!({
-        "code":200,
-        "message": "query success"
-    });
+
     match repository {
         Ok(repository) => {
-            let _result_from_email : Vec<String> = repository.get_reservation_from_email(reservation_email).await.unwrap();
-            (StatusCode::CREATED, Json(success))
+            let result_from_email: Vec<Value> = repository.get_reservation_from_email(reservation_email).await.unwrap();
+            Ok((StatusCode::CREATED, Json(result_from_email)))
         }
-        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(error)),
+        Err(error) => {
+            let error_json: Value = json!({
+                "error": {
+                    "code": 404,
+                    "message": error.to_string()
+             }
+            });
+            Err((StatusCode::BAD_REQUEST, Json(error_json)))
+        }
     }
 }
