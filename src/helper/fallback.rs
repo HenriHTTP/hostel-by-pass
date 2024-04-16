b42e04cda7dc4a628b77ea56488784d5ef2f6f70
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-16 18:58:41 -0300

    refactor: implement send message error for fallback  returns

--- src/helper/fallback.rs
+++ src/helper/fallback.rs
@@ -1,14 +1,9 @@
+use crate::helper::message_json;
 use axum::http::StatusCode;
 use axum::Json;
-use serde_json::json;
 use serde_json::Value;
 
 pub async fn api_fallback() -> (StatusCode, Json<Value>) {
-    let body_response: Value = json!({
-        "error": {
-        "code": 404,
-        "message": "Route not found. Please check the URL and try again later."
-        }
-    });
-    (StatusCode::NOT_FOUND, Json(body_response))
+    let error: Value = message_json::send_message_error(404, "Route not found. Please check the URL and try again later.").await;
+    (StatusCode::NOT_FOUND, Json(error))
 }
