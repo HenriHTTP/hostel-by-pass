ce1d79e08bf66fa6a95dac84db1b1e5d2169ef6b
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-23 20:29:50 -0300

    feat: add mod get reservation check out date

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,3 +1,4 @@
 pub mod create_reservation;
-pub mod get_reservation_by_email;
-pub mod get_reservation_by_date;
+pub mod get_reservation_email;
+pub mod get_reservation_check_in_date;
+pub mod get_reservation_check_out_date;
