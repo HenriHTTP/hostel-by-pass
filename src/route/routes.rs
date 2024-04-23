6159d0813f7ebf7efdd98d57ff537277ea40cddb
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-04-22 21:25:40 -0300

    feat: add route check in date

--- src/route/routes.rs
+++ src/route/routes.rs
@@ -2,6 +2,7 @@ use axum::Router;
 use axum::routing::post;
 use crate::controller::create_reservation::create_reservation;
 use crate::controller::get_reservation_by_email::get_reservation_by_email;
+use crate::controller::get_reservation_by_date::get_reservation_by_date; 
 use crate::helper::fallback::api_fallback;
 
 
@@ -9,5 +10,6 @@ pub fn routes () -> Router{
     Router::new()
         .route("/create_reservation",post(create_reservation))
         .route("/email", post(get_reservation_by_email))
+        .route("/check_in_date", post(get_reservation_by_date))
         .fallback(api_fallback)
 }
